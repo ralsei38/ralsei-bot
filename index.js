3beb1a54c3cecbd49267b3bf90ef10702f1157c4
ralsei38 <fontana.theo.me@gmail.com>
2021-11-26 16:41:14 +0100

    index file bot

---
+++ index.js
@@ -0,0 +1,20 @@
+// READ THE DOC ! https://discord.js.org/#/docs/main/stable/general/welcome
+// Require the necessary discord.js classes
+// destructuration of the discord.js req into theses variables
+const { Client, Intents } = require('discord.js');
+
+//destructuring config.json file finding corresponding token entry and writing to the token variable.
+const { token } = require('./config.json')
+
+// Create a new client instance
+//ARGS: 
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// .once to run code only on time...
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async)
+// Logs the client in, establishing a WebSocket connection to Discord.
+client.login(token);
