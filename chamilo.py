15c94ae5ba1e39d301bfb082515578431a5d9b93
ralsei38 <fontana.theo.me@gmail.com>
2021-11-29 21:45:27 +0100

    using most recent BS4 methods replacing the old method calls

--- chamilo.py
+++ chamilo.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox import service
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
@@ -13,9 +14,7 @@ import json
 load_dotenv()
 #--- const
 course = {
-    
     # 'course_name': (coef,grade)
-    
 }
 #--- login data
 username_txt = str(os.getenv('CHAMILO_USERNAME'))
@@ -26,6 +25,8 @@ firefox_options = webdriver.FirefoxOptions()
 ua = UserAgent()
 fake_user_agent = ua.random
 firefox_options.add_argument(f"user-agent={fake_user_agent}")
+firefox_options.add_argument("--log-level=3")
+firefox_options.add_argument("--headless")
 #--- urls
 chamilo_login_url = "https://cas-uga.grenet.fr/login"
 chamilo_grade_url = "https://scolarite-informatique.iut2.univ-grenoble-alpes.fr/app/ficheEtudiant.php"
@@ -34,12 +35,11 @@ chamilo_grade_url = "https://scolarite-informatique.iut2.univ-grenoble-alpes.fr/
 #--- driver binary setup & driver implementation
 firefox_service = Service("/usr/local/bin/geckodriver")
 driver = webdriver.Firefox(service=firefox_service, options=firefox_options)
-
 #--- logging in
 driver.get(chamilo_login_url)
-username = driver.find_element_by_id('username')
-password = driver.find_element_by_id('password')
-submit = driver.find_element_by_name('submit')
+username = driver.find_element(By.ID, 'username')
+password = driver.find_element(By.ID, 'password')
+submit = driver.find_element(By.NAME, 'submit')
 username.send_keys(username_txt)
 password.send_keys(password_txt)
 submit.click()
@@ -50,17 +50,17 @@ tbody_tag = my_tasty_soup.find('table').find('tbody')
 rows = tbody_tag.find_all('tr')[3:]
 
 for row in rows:
-    course_name = row.find('td').text.strip()
+    course_name = (row.find('td').text.strip())
     course[course_name] = (row.findAll('td')[-3].text.strip(), row.findAll('td')[-2].text.strip())
     row = row.next_sibling
     if row is None:
-        print("bybye\n")
+        print("End Of Parse\n")
         break
 
 must_update = 0
 with open('grade.json', 'r') as f:
     previous_course_json = f.read()
-    if previous_course_json == json.dumps(course):
+    if previous_course_json == json.dumps(course, indent=4, ensure_ascii=False).encode('utf8').decode():
         print("up to date")
         exit(0)
     else:
@@ -69,11 +69,5 @@ with open('grade.json', 'r') as f:
 
 if must_update:
     with open('grade.json', 'w') as f:
-        current_course_json = json.dumps(course)
+        current_course_json = json.dumps(course, indent=4, ensure_ascii=False).encode('utf8').decode()
         f.write(current_course_json)
-
-# for key,value in course.items():
-#     print("name: {}".format(key))
-#     print("coef: {}".format(value[0]))
-#     print("grade: {}".format(value[1]))
-#     print("#"*10)
