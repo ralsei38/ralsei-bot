27c1ccfbe0382d2fc9e4f1bf49b868d4166a2a03
ralsei38 <fontana.theo.me@gmail.com>
2021-12-07 11:29:00 +0100

    [FIX] added necessary Builders

--- deploy-command.js
+++ deploy-command.js
@@ -1,10 +1,18 @@
 // to build your own /commands
-const {Builder} = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 // builder dependencies
 const { REST } = require('@discordjs/rest');
-const { Routes } = require('@discordjs/route');
-
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
 const commands = [
-	new Builder().setName('ping').setDescription('We calling this commands, ralsei responds \'pong\''),
-	
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('grade').setDescription('Ralsei says \'yup\' if there is a new grade'),
 ]
+	.map(command => command.toJSON());
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+.then(() => console.log('Successfully registered application commands.'))
+.catch(console.error);
